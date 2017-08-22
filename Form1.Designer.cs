4f40851fe16190f2187f1a503c4f8f8bc3285cd7
プラジュワル ニンマガッダ <prajcindav@outlook.com>
2017-08-22 15:49:10 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace DotNetSample_64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPower = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelEnergy = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_wavalength = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_status = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Power =";
+            // 
+            // labelPower
+            // 
+            this.labelPower.AutoSize = true;
+            this.labelPower.Location = new System.Drawing.Point(64, 85);
+            this.labelPower.Name = "labelPower";
+            this.labelPower.Size = new System.Drawing.Size(13, 13);
+            this.labelPower.TabIndex = 1;
+            this.labelPower.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "START LOGGING";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // labelEnergy
+            // 
+            this.labelEnergy.AutoSize = true;
+            this.labelEnergy.Location = new System.Drawing.Point(64, 106);
+            this.labelEnergy.Name = "labelEnergy";
+            this.labelEnergy.Size = new System.Drawing.Size(13, 13);
+            this.labelEnergy.TabIndex = 6;
+            this.labelEnergy.Text = "?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Joule",
+            "Watt",
+            "Volt"});
+            this.comboBox1.Location = new System.Drawing.Point(112, 274);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Energy =";
+            // 
+            // comboBox_wavalength
+            // 
+            this.comboBox_wavalength.FormattingEnabled = true;
+            this.comboBox_wavalength.Items.AddRange(new object[] {
+            "780",
+            "980",
+            "1064"});
+            this.comboBox_wavalength.Location = new System.Drawing.Point(14, 274);
+            this.comboBox_wavalength.Name = "comboBox_wavalength";
+            this.comboBox_wavalength.Size = new System.Drawing.Size(83, 21);
+            this.comboBox_wavalength.TabIndex = 10;
+            this.comboBox_wavalength.SelectedIndexChanged += new System.EventHandler(this.comboBox_wavalength_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 25);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(110, 322);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(69, 13);
+            this.label_status.TabIndex = 12;
+            this.label_status.Text = "Label_Status";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 139);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(354, 128);
+            this.textBox1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(95, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "CLEAR BUFFER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(176, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 35);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Rewrite PowerLog.txt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(283, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Always Archive";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(208, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 369);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox_wavalength);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelEnergy);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelPower);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPower;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelEnergy;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_wavalength;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DotNetSample_64
+{
+    public partial class Form1 : Form
+    {
+        private Thorlabs.PM100D.PM100D pm100d;
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        double powerValue;
+        double energyvalue;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = comboBox1.FindStringExact("Watt");
+            comboBox_wavalength.SelectedIndex = comboBox_wavalength.FindStringExact("780");
+
+            try
+            {
+                pm100d = new Thorlabs.PM100D.PM100D("USB0::0x1313::0x8075::P5000256::INSTR", true, true);  //  For valid Ressource_Name see NI-Visa documentation.
+
+            }
+
+            catch (BadImageFormatException bie)
+            {
+                labelPower.Text = bie.Message;
+            }
+            catch (NullReferenceException nre)
+            {
+                labelPower.Text = nre.Message;
+            }
+            catch (ExternalException ex)
+            {
+                labelPower.Text = ex.Message;
+            }
+            finally
+            {
+                if (pm100d != null)
+                    pm100d.Dispose();
+            }
+        }
+
+
+
+        public void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            label2.Text = DateTime.Now.ToString();
+
+            List<string> strdata = new List<string>();
+
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+
+                    pm100d.measEnergy(out energyvalue);
+                    labelEnergy.Text = energyvalue.ToString();
+
+                    break;
+
+                case 1:
+                    pm100d.measPower(out powerValue);
+                    labelPower.Text = powerValue.ToString();
+                    break;
+            }
+
+
+            strdata.Add(label2.Text);
+            strdata.Add(labelPower.Text);
+            textBox1.Text += strdata[0] + "," + strdata[1] + "\r\n";
+
+
+            if (checkBox1.Checked)
+            {
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\prajwal.nimmagadda\Desktop\Thorlabs\ThorlabsData\PowerArchive.xlsx", true))
+                {
+                    file.WriteLine(strdata[0] + "," + strdata[1] + "\n");
+                }
+            }
+            else
+            {
+            }
+        }
+
+        private void comboBox_wavalength_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pm100d = new Thorlabs.PM100D.PM100D("USB0::0x1313::0x8075::P5000256::INSTR", false, false);  //  For valid Ressource_Name see NI-Visa documentation.
+
+            double wavelength0 = 780;
+            double wavelength1 = 980;
+            double wavelength2 = 1064;
+
+            switch (comboBox_wavalength.SelectedIndex)
+
+            {
+                case 0:
+                    pm100d.setWavelength(wavelength0);
+                    break;
+
+                case 1:
+                    pm100d.setWavelength(wavelength1);
+                    break;
+
+                case 2:
+                    pm100d.setWavelength(wavelength2);
+                    break;
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                pm100d = new Thorlabs.PM100D.PM100D("USB0::0x1313::0x8075::P5000256::INSTR", false, false);  //  For valid Ressource_Name see NI-Visa documentation.
+                if (button1.Text == "STOP LOGGING")
+                {
+                    button1.Text = "START LOGGING";
+                    timer1.Enabled = false;
+                }
+                else
+                {
+                    button1.Text = "STOP LOGGING";
+                    timer1.Enabled = true;
+                }
+            }
+            catch(ExternalException exz)
+            {
+                textBox2.Text = exz.Message;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Clear Buffer
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            short bb;
+
+            pm100d.readRegister(20, out bb);
+
+            label_status.Text = bb.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            System.IO.File.WriteAllText(@"C:\Users\prajwal.nimmagadda\Desktop\Thorlabs\ThorlabsData\PowerLog.txt", text);
+
+        }
+
+    }
+}
+
+
